ee7a9241e2334ba3620e489d7bc9b6b6ead8cc4d
Colton Mizeur <cmmtvd@umsystem.edu>
2026-04-27 13:06:40 -0500

    updated databse schemas

--- git/2830_FlowState_MERN_Project/server/src/models/Task.js
+++ git/2830_FlowState_MERN_Project/server/src/models/Task.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
+  startDate: { type: Date },
   dueDate: { type: Date },
   priority: { type: String, enum: ['Low', 'Medium', 'High'] },
   status: { type: String, enum: ['To Do', 'In Progress', 'Done'], default: 'To Do' },
